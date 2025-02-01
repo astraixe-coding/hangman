f1138d24f313e310d7aa3cd5ddd35d7e78458f0c
astraixe-coding <classifiedcontentonyt@gmail.com>
2025-02-01 21:02:06 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hangman-game-jg',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[], 
+    description="A quick and easy way to play hangman!",
+    author="Your Name",
+    author_email="your_email@example.com",
+    url="https://github.com/yourusername/my_module"
+)
